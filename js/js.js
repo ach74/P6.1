422bd40717d79da13dbf4d4127ba96130e5ffbe1
ach74 <32370243+ach74@users.noreply.github.com>
2019-02-26 01:42:36 +0100

    msj

--- js/js.js
+++ js/js.js
@@ -5,10 +5,19 @@ $(document).ready(function(){
 	$("#fade").click(function(){
 		$("#panel-fade").fadeToggle();
 	});
+	$("#fade-2").click(function(){
+		$("#panel-fade-2").fadeToggle();
+	});
+	$("#fade-3").click(function(){
+		$("#panel-fade-3").fadeToggle();
+	});
 	$("#animation").click(function(){
 		$("#panel-animation").animate({
 			height: 'toggle'
 		});
+	});
+	$("#button-addon1").click(function(){
+		window.location.href = "filtro.html";
 	});
 });
 
